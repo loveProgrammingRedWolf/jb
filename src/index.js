c4eda152949a9ee1225451d269ce6ff84d881278
刘琼 <jones.liu@ximalaya.com>
2020-07-21 17:42:10 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from  'react-dom';
+import * as Angular from 'angular';
+import * as Jquery from 'jquery';
+import * as Vue from 'vue';
+
+const JB = {
+  '无坚不摧模式': {
+    React,
+    ReactDOM
+  },
+  '续航模式': Angular,
+  '战神模式': Jquery,
+  '低调模式': Vue
+}
+
+export default JB;
